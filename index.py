598fb16fe11328a589260244870e9ecf6feb6809
napalm255 <napalm255@gmail.com>
2017-12-18 13:07:25 -0500

    adding table

--- index.py
+++ index.py
@@ -25,22 +25,45 @@ def handler(event, context):
     """Lambda handler."""
     # pylint: disable=unused-argument
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
     logger.info(event)
 
     header = {'Content-Type': 'application/json'}
 
+    table_name = 'traffic'
+    table_scheme = ("id INT NOT NULL AUTO_INCREMENT,"
+                    "timestamp TIMESTAMP NOT NULL,"
+                    "origin VARCHAR(255) NOT NULL,"
+                    "destination VARCHAR(255) NOT NULL,"
+                    "distance_miles FLOAT,"
+                    "distance_meters FLOAT,"
+                    "duration INT,"
+                    "duration_in_traffic INT")
+
     with connection.cursor() as cursor:
+        # check if database exists
         cursor.execute('show databases')
         databases = cursor.fetchall()
         logging.info(databases)
+        # create database if it does not exist
         if (DATA['db_name'],) not in databases:
             cursor.execute('CREATE DATABASE %s' % DATA['db_name'])
             connection.commit()
+        # use the database
         connection.select_db(DATA['db_name'])
+        # check if table exists
         cursor.execute('show tables')
         tables = cursor.fetchall()
         logging.info(tables)
+        # create table if it does not exist
+        if (table_name,) not in tables:
+            cursor.execute('CREATE TABLE commute (%s)' % table_scheme)
+            connection.commit()
+        # select existing records
+        cursor.execute('SELECT * FROM commute')
+        records = cursor.fetchall()
+        logging.info(records)
+        # insert new record
 
     message = {'databases': databases,
                'database': {
