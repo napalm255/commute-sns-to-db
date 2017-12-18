eb4e81493e481793924f47720f58254ef473aa89
napalm255 <napalm255@gmail.com>
2017-12-18 10:26:43 -0500

    added vpc config. add params from ssm. test py code.

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 """Commute SNS to Database."""
 
 import logging
+import os
 import json
-import datetime
 import pymysql
 
 
@@ -15,12 +15,19 @@ def handler(event, context):
 
     header = {'Content-Type': 'application/json'}
 
-    data = {
-        'timestamp': datetime.datetime.utcnow().isoformat(),
-        'pymysql': str(pymysql)
-    }
+    data = {'db_host': os.environ['DATABASE_HOST'],
+            'db_user': os.environ['DATABASE_USER'],
+            'db_pass': os.environ['DATABASE_PASS'],
+            'db_name': os.environ['DATABASE_NAME']}
     logging.info(data)
 
+    connection = pymysql.connect(host=data['db_host'],
+                                 user=data['db_user'],
+                                 password=data['db_pass'])
+    results = connection.cursor().execute('show tables')
+
+    message = {'tables': results}
+
     return {'statusCode': 200,
-            'body': json.dumps(data),
+            'body': json.dumps(message),
             'headers': header}
